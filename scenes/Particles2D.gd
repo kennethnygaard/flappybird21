e4912f23330cec0df9b3377cff6d6c0a2e1870da
Kenneth Nygaard <kennethnygaard@Kenneths-MacBook-Air.local>
2021-10-29 10:25:07 +0200

    added forground rock particles + played around with smoke effect

---
+++ scenes/Particles2D.gd
@@ -0,0 +1,8 @@
+extends Particles2D
+
+func _ready():
+	global_position.y = global_position.y + 20
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
